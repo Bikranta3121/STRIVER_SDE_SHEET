353e3232104e46e486f58ca17548fcba876564e9
BIKRANTA KUMAR BEHERA <106620478+Bikranta3121@users.noreply.github.com>
2023-08-25 23:33:18 +0530

    Update 3 sum problem.cpp

--- ARRAY/HARD/3 sum problem.cpp
+++ ARRAY/HARD/3 sum problem.cpp
@@ -3,28 +3,35 @@
         vector<vector<int>> threeSum(vector<int>& nums) {
             sort(nums.begin(),nums.end());
             int i=0;
-            map<pair<int,int>,int> s;
+            int curr=INT_MIN;
             vector<vector<int>> ans;
             while(i<=nums.size()-3 and nums[i]<=0 )
             {
                 int x=-nums[i];
                 int start=i+1;
                 int end=nums.size()-1;
-                while(start<nums.size()-1 and start<end)
+                while(start<nums.size()-1 and start<end and curr!=nums[i])
                 {
                     if(nums[start]+nums[end]>x) end--;
                     else if(nums[start]+nums[end]<x) start++;
                     else
                     {
-                        if(s.find({nums[start],nums[end]})==s.end())
-                        {
                         ans.push_back({nums[i],nums[start],nums[end]});
-                        s[{nums[start],nums[end]}]=nums[i];
+                        int currr=nums[start];
+                    
+                        while(currr==nums[start]  and start<end)
+                        {
+                            start++;
                         }
-                        start++;
-                        end--;
+                         currr=nums[end];
+                    
+                        while(currr==nums[end]  and end>start)
+                        {
+                            end--;
+                        }                 
                     } 
                 }  
+                curr=nums[i];
                 i++;          
             }
             // if(i<=nums.size()-3 and nums[i]==0)
